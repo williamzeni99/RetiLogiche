3afd9d1d8f32ddf7f858388c2717a66a57067426
williamzeni <william.zeni@mail.polimi.it>
2021-04-17 13:23:51 +0200

    first commit

---
+++ Reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/29/2021 10:52:26 AM
+-- Design Name: 
+-- Module Name: project_reti_logiche - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_unsigned.all;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity project_reti_logiche is
+    Port ( i_clk : in STD_LOGIC;
+           i_rst : in STD_LOGIC;
+           i_start : in STD_LOGIC;
+           i_data : in std_logic_vector(7 downto 0);
+           o_address : out std_logic_vector(15 downto 0);
+           o_done : out STD_LOGIC;
+           o_en : out STD_LOGIC;
+           o_we : out STD_LOGIC;
+           o_data : out std_logic_vector(7 downto 0));
+end project_reti_logiche;
+
+architecture Behavioral of project_reti_logiche is
+
+begin
+
+
+end Behavioral;
