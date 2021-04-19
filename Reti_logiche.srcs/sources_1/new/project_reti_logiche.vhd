2a2e883ef2dec2a645b990bd825c2c32a348d9c2
williamzeni <william.zeni@mail.polimi.it>
2021-04-19 17:21:15 +0200

    Primo process fatto

--- Reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ Reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -44,8 +44,9 @@ end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
     type state_type is (START, INIT, GET_DIM, GET_MINDELTA, NEW_PIXEL, DONE, MEMRW);
-    signal next_state, prev_state: state_type;
+    signal next_state,curr_state, prev_state: state_type;
 
+    --signal to store
     signal max_pixel_value: integer range 0 to 255 := 0;
     signal min_pixel_value: integer range 0 to 255 := 255;
     signal delta_value: integer range 0 to 255 := 0;
@@ -53,7 +54,19 @@ architecture Behavioral of project_reti_logiche is
     signal new_pixel_value: integer range 0 to 255:=0;
     signal temp_pixel: integer range 0 to 255 := 0;
     signal curr_address: std_logic_vector(15 downto 0);
-    signal dim_address: std_logic_vector(15 downto 0);
+    signal dim_address: std_logic_vector(15 downto 0):
+
+    --signal to work with
+    signal max_pixel_value_cp: integer range 0 to 255 := 0;
+    signal min_pixel_value_cp: integer range 0 to 255 := 255;
+    signal delta_value_cp: integer range 0 to 255 := 0;
+    signal shift_level_cp: integer range 0 to 8:= 0;
+    signal new_pixel_value_cp: integer range 0 to 255:=0;
+    signal temp_pixel_cp: integer range 0 to 255 := 0;
+    signal curr_address_cp: std_logic_vector(15 downto 0);
+    signal dim_address_cp: std_logic_vector(15 downto 0);
+
+    --signal to update output
     signal o_address_next: std_logic_vector(15 downto 0);
     signal o_done_next: std_logic_vector(15 downto 0);
     signal o_en_next: std_logic_vector(15 downto 0);
@@ -64,9 +77,35 @@ begin
   process(i_clk,i_rst)
     begin
       if(i_rst='1')
-        --reset
-      elsif(rising_edge(i_clk))
-        --setvalori
+        max_pixel_value<=0;
+        min_pixel_value<=0;
+        delta_value<=0;
+        shift_level<=0;
+        new_pixel_value<=0;
+        temp_pixel<=0;
+        curr_address<=(others => '0');
+        dim_address<=(others => '0');
+        curr_state<=START;
+        
+      elsif(rising_edge(i_clk)) then
+        o_done <= o_done_next;
+        o_en <= o_en_next;
+        o_we <= o_we_next;
+        o_data <= o_data_next;
+        o_address <= o_address_next;
+
+        max_pixel_value<=max_pixel_value_cp;
+        min_pixel_value<=min_pixel_value_cp;
+        delta_value<=delta_value_cp;
+        shift_level<=shift_level_cp;
+        new_pixel_value<=new_pixel_value_cp;
+        temp_pixel<=temp_pixel_cp;
+        curr_address<=curr_address_cp;
+        dim_address<=dim_address_cp;
+
+        curr_state<=next_state;
+
+
       end if;
   end process;
 
