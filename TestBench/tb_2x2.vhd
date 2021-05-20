35daea701feb3d4fa86b1957848b40ee1dd1977e
williamzeni <william.zeni@mail.polimi.it>
2021-05-20 13:16:48 +0200

    andato avanti

--- TestBench/tb_2x2.vhd
+++ TestBench/tb_2x2.vhd
@@ -21,17 +21,17 @@ signal   mem_we                 : std_logic;
 type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);
 
 
-signal RAM: ram_type := (0 => std_logic_vector(to_unsigned(  2  , 8)), 
-                         1 => std_logic_vector(to_unsigned(  2  , 8)), 
-                         2 => std_logic_vector(to_unsigned(  46  , 8)),  
-                         3 => std_logic_vector(to_unsigned(  131  , 8)), 
-                         4 => std_logic_vector(to_unsigned(  62  , 8)), 
-                         5 => std_logic_vector(to_unsigned(  89  , 8)), 
-                         others => (others =>'0'));         
-			 -- Expected Output  6 -> 0                         
-			 -- Expected Output  7 -> 255                         
-			 -- Expected Output  8 -> 64                         
-			 -- Expected Output  9 -> 172                         
+signal RAM: ram_type := (0 => std_logic_vector(to_unsigned(  2  , 8)),
+                         1 => std_logic_vector(to_unsigned(  2  , 8)),
+                         2 => std_logic_vector(to_unsigned(  46  , 8)),
+                         3 => std_logic_vector(to_unsigned(  131  , 8)),
+                         4 => std_logic_vector(to_unsigned(  62  , 8)),
+                         5 => std_logic_vector(to_unsigned(  89  , 8)),
+                         others => (others =>'0'));
+			 -- Expected Output  6 -> 0
+			 -- Expected Output  7 -> 255
+			 -- Expected Output  8 -> 64
+			 -- Expected Output  9 -> 172
 
 component project_reti_logiche is
 port (
@@ -52,13 +52,13 @@ begin
 UUT: project_reti_logiche
 port map (
           i_clk      	=> tb_clk,
-          i_start       => tb_start,
+          i_start     => tb_start,
           i_rst      	=> tb_rst,
           i_data    	=> mem_o_data,
           o_address  	=> mem_address,
-          o_done      	=> tb_done,
-          o_en   	=> enable_wire,
-          o_we 		=> mem_we,
+          o_done      => tb_done,
+          o_en   	    => enable_wire,
+          o_we 		    => mem_we,
           o_data    	=> mem_i_data
           );
 
@@ -85,7 +85,7 @@ end process;
 
 
 test : process is
-begin 
+begin
     wait for 100 ns;
     wait for c_CLOCK_PERIOD;
     tb_rst <= '1';
@@ -102,9 +102,9 @@ begin
     wait until tb_done = '0';
     wait for 100 ns;
 
-    -- Immagine originale =  [46, 131, 62, 89]  
-    -- Immagine di output =  [0, 255, 64, 172]  
-    
+    -- Immagine originale =  [46, 131, 62, 89]
+    -- Immagine di output =  [0, 255, 64, 172]
+
     assert RAM(6) = std_logic_vector(to_unsigned( 0 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  0  found " & integer'image(to_integer(unsigned(RAM(6))))  severity failure;
     assert RAM(7) = std_logic_vector(to_unsigned( 255 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  255  found " & integer'image(to_integer(unsigned(RAM(7))))  severity failure;
     assert RAM(8) = std_logic_vector(to_unsigned( 64 , 8)) report "TEST FALLITO (WORKING ZONE). Expected  64  found " & integer'image(to_integer(unsigned(RAM(8))))  severity failure;
@@ -113,6 +113,4 @@ begin
     assert false report "Simulation Ended! TEST PASSATO" severity failure;
 end process test;
 
-end projecttb; 
-
-
+end projecttb;
